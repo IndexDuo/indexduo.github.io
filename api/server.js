0d28cd7a759b02ed690e6a7faf3461d76f805f2b
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-04-11 07:48:44 +0000

    Apr 11, 2024, 3:48 AM

--- api/server.js
+++ api/server.js
@@ -1,49 +1,19 @@
-// server.js
-import express from 'express';
 import axios from 'axios';
-import dotenv from 'dotenv';
-import process from 'process';
-import cors from 'cors';
 
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-const corsOptions = {
-    origin: 'http://localhost:5173'&&'https://indexduo.me', // Allow requests from your React app
-};
-
-app.use(cors(corsOptions)); // Use CORS middleware with the specified options
-
-app.use(express.json()); // For parsing application/json
-
-// Define a route in your Express server
-app.get('/api/status', async (req, res) => {
+export default async (req, res) => {
     const url = 'https://betteruptime.com/api/v2/status-pages';
-
+    
     try {
-        // Make an outgoing HTTP request from your server to the Better Uptime API
         const response = await axios.get(url, {
             headers: {
-                "Content-Type": "application/json",
-                // Use your actual API token from your .env file, do not hard-code it
-                "Authorization": `Bearer ${process.env.BETTER_UPTIME_API_KEY}`
-            }
+                'Authorization': 'Bearer BHVR9pHt8nLFCGmUVWXx8V1e',
+                'Content-Type': 'application/json',
+            },
         });
-
-        // Process and send the response back to your front-end
-        if (response.data && response.data.data && response.data.data.length > 0) {
-            res.json({ statusPages: response.data.data });
-        } else {
-            res.status(404).json({ error: 'No status page data found' });
-        }
+        
+        res.status(200).json(response.data);
     } catch (error) {
-        console.error('Error fetching status from Better Uptime:', error);
-        res.status(500).json({ error: 'Failed to fetch status page data' });
+        console.error('API call failed:', error);
+        res.status(500).json({ error: 'Failed to fetch data' });
     }
-});
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
+};
